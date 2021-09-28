9f912032d9df0b16a93f6e983ead1ad6fdf2d51f
Mohamad EL-Chanti <m.elchanti@gmail.com>
2021-09-27 19:45:43 -0700

    finished lesson 5

--- lesson_5/practiceProblems.js
+++ lesson_5/practiceProblems.js
@@ -37,7 +37,7 @@ console.log (arr2[1].third[0]);
 console.log (arr3[2].third[0][0]);
 console.log (obj1.b[1]);
 console.log(Object.keys(obj2.third)[0]);
-*/
+
 console.log ('\nQuestion 4');
 
 let arr1 = [1, [2, 3], 4];
@@ -139,3 +139,139 @@ console.log (arr.map(array => {
 }));
 
 console.log(arr);
+
+console.log('\nQuestion 11');
+
+let arr = [{ a: 1 }, { b: 2, c: 3 }, { d: 4, e: 5, f: 6 }];
+
+let copyArr = arr.map(obj => {
+  let newObj = Object.assign({}, obj);
+
+  Object.keys(newObj).forEach(key => newObj[key] += 1);
+  return newObj;
+});
+
+console.log (arr);
+console.log (copyArr);
+
+console.log ('\nQuestion 12');
+
+let arr = [[2], [3, 5, 7], [9], [11, 15, 18]];
+
+let newArr = arr.map(array => {
+  return array.filter (num => num % 3 === 0);
+});
+
+console.log (arr);
+console.log (newArr);
+
+console.log ('\nQuestion 13');
+
+let arr = [[1, 6, 7], [1, 5, 3], [1, 8, 3]];
+
+let newArr = [...arr].sort ((a, b) => {
+  let aSumOdd = a.filter(num => num % 2 !== 0).reduce ((accum, num) => {
+    return accum + num;
+  }, 0);
+  let bSumOdd = b.filter(num => num % 2 !== 0).reduce ((accum, num) => {
+    return accum + num;
+  }, 0);
+
+  return aSumOdd - bSumOdd;
+});
+
+console.log (arr);
+console.log (newArr);
+
+console.log ('\nQuestion 14');
+
+let obj = {
+  grape: { type: 'fruit', colors: ['red', 'green'], size: 'small' },
+  carrot: { type: 'vegetable', colors: ['orange'], size: 'medium' },
+  apple: { type: 'fruit', colors: ['red', 'green'], size: 'medium' },
+  apricot: { type: 'fruit', colors: ['orange'], size: 'medium' },
+  marrow: { type: 'vegetable', colors: ['green'], size: 'large' },
+};
+
+let newObj = Object.values(obj).map (value => {
+  if (value.type === 'fruit') {
+    return value.colors.map (color => color[0].toUpperCase() + color.slice(1));
+  }
+
+  return value.size.toUpperCase();
+});
+
+console.log (obj);
+console.log (newObj);
+
+console.log ('\nQuestion 15');
+
+let arr = [
+  { a: [1, 2, 3] },
+  { b: [2, 4, 6], c: [3, 6], d: [4] },
+  { e: [8], f: [6, 10] },
+];
+
+let newArr = arr.filter (obj => {
+  return Object.values(obj).every (array => {
+    return array.every (num => num % 2 === 0);
+  });
+});
+
+console.log (arr);
+console.log (newArr);
+
+console.log ('\nQuestion 16');
+let arr = [['a', 1], ['b', 'two'], ['sea', {'c': 3}], ['D', ['a', 'b', 'c']]];
+
+// expected return value of function call
+// { a: 1, b: 'two', sea: { c: 3 }, D: [ 'a', 'b', 'c' ] }
+
+let obj = {};
+
+arr.forEach (nestedArray => {
+  obj[nestedArray[0]] = nestedArray[1];
+});
+
+console.log (obj);
+*/
+
+/* Question 17
+Problem: 1- Return a UUID string
+2- No Arguments
+3- UUID string has letters that could be 0-9 or a-f
+4- 8-4-4-4-12 pattern
+
+Examples: Given
+
+Data Structures: Array that maps 0-16 to the letters in UUID
+
+Algorithm: 1. Randomly generate an integer between 0-16
+2. Create a array that is used to map the random number to a letter
+3. Repeat for 32 letters
+4. Concatenate the 32 letters in a UUID format
+
+*/
+
+function UUID () {
+  let dictionary = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 'a', 'b', 'c', 'd', 'e', 'f'];
+
+  let UUID = '';
+
+  for (let index = 0; index < 32; index += 1) {
+    UUID = UUID + dictionary[randomNumber()];
+  }
+
+  console.log (UUID + ' ' + UUID.length);
+  UUID = UUID.slice (0,8) + '-' + UUID.slice (8, 12) + '-' + UUID.slice (12, 16) + '-' +
+        UUID.slice(16, 20) + '-' + UUID.slice (20);
+  
+  return UUID;
+
+}
+
+function randomNumber () {
+  return Math.floor (Math.random() * 16);
+}
+
+console.log (UUID());
